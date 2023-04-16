16950fee00e4c2f838bfaea457b311f759525ad6
Debopriyo Goswami <126260211+debo004@users.noreply.github.com>
2023-04-16 13:36:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// let menu = document.querySelector('#menu-bar');
+// let navbar = document.querySelector('.navbar');
+
+// document.querySelector("#menu-bar").addEventListener("click", () => {
+//     document.querySelector("#menu-bar").classList.toggle("");
+// });
+
+document.querySelector("#menu-bar").addEventListener("click", () => {
+  document.querySelector(".navbar").classList.toggle("active");
+});
